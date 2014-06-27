305f6e478f1f32908a56079f332592f9905e6ccb
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-27 00:02:40 -0400

    a couple of enhancements

--- src/modelo.erl
+++ src/modelo.erl
@@ -5,6 +5,15 @@
 %-fields([]).
 
 search(Table, Values) ->
-	{fields, Fields} = proplists:lookup(fields, Table:module_info(attributes)),
-	Params = [ {P, V} || {P, {ok, V}} <- [ {I, maps:find(K, Values)} || {I, K} <- lists:zip(lists:seq(2, length(Fields)+1), Fields)]],
+	Params = [ {P, V} || {P, {ok, V}} <- [ {I, maps:find(K, Values)} || {I, K} <- getFieldOffsets(Table)]],
 	erlang:make_tuple(length(Fields)+1, '_', [{1, Table}|Params]).
+
+recordToMap(Table, Record) ->
+	maps:from_list([ {K, element(I, Record)} || {I, K} <- getFieldOffsets(Table)]).
+
+getFieldOffsets(Table) ->
+	lists:zip(lists:seq(2, length(Fields)+1), getFields(Table)).
+
+getFields(Table) ->	
+	{fields, Fields} = proplists:lookup(fields, Table:module_info(attributes)),
+	Fields.
