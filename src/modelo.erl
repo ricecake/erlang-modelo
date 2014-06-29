8bce4b461e342aa5878fbca5271e5044614a0b57
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-28 22:49:36 -0400

    fix some small bugs

--- src/modelo.erl
+++ src/modelo.erl
@@ -1,6 +1,6 @@
 -module(modelo).
 
--export([search/2]).
+-export([init/2, find/2, recordToMap/2, search/2]).
 
 %-fields([]).
 
@@ -8,20 +8,20 @@ init(Table, Opts) ->
 	mnesia:create_table(Table, [{fields, getFields(Table)}|Opts]).
 
 find(Table, Key) ->
-	[KeyField | _Rest] = getFields(Table),
-	Match = erlang:make_tuple(2, [Table, KeyField]),
+	Match = erlang:make_tuple(2, [Table, Key]),
 	mnesia:transaction(fun()-> mnesia:read(Match) end).
 
 search(Table, Values) ->
 	Params = [ {P, V} || {P, {ok, V}} <- [ {I, maps:find(K, Values)} || {I, K} <- getFieldOffsets(Table)]],
-	Pattern = erlang:make_tuple(length(Fields)+1, '_', [{1, Table}|Params]),
+	Pattern = erlang:make_tuple(length(Params)+1, '_', [{1, Table}|Params]),
 	mnesia:transaction(fun()-> mnesia:match_object(Pattern) end).
 
 recordToMap(Table, Record) ->
 	maps:from_list([ {K, element(I, Record)} || {I, K} <- getFieldOffsets(Table)]).
 
 getFieldOffsets(Table) ->
-	lists:zip(lists:seq(2, length(Fields)+1), getFields(Table)).
+	Fields = getFields(Table),
+	lists:zip(lists:seq(2, length(Fields)+1), Fields).
 
 getFields(Table) ->	
 	{fields, Fields} = proplists:lookup(fields, Table:module_info(attributes)),
