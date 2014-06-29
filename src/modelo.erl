f708a068967a73a19e9a4bc6e7116e183cc3bde6
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-28 22:44:07 -0400

    flesh out some more util functions

--- src/modelo.erl
+++ src/modelo.erl
@@ -4,13 +4,18 @@
 
 %-fields([]).
 
+init(Table, Opts) -> 
+	mnesia:create_table(Table, [{fields, getFields(Table)}|Opts]).
+
 find(Table, Key) ->
 	[KeyField | _Rest] = getFields(Table),
-	erlang:make_tuple(2, [Table, KeyField]).
+	Match = erlang:make_tuple(2, [Table, KeyField]),
+	mnesia:transaction(fun()-> mnesia:read(Match) end).
 
 search(Table, Values) ->
 	Params = [ {P, V} || {P, {ok, V}} <- [ {I, maps:find(K, Values)} || {I, K} <- getFieldOffsets(Table)]],
-	erlang:make_tuple(length(Fields)+1, '_', [{1, Table}|Params]).
+	Pattern = erlang:make_tuple(length(Fields)+1, '_', [{1, Table}|Params]),
+	mnesia:transaction(fun()-> mnesia:match_object(Pattern) end).
 
 recordToMap(Table, Record) ->
 	maps:from_list([ {K, element(I, Record)} || {I, K} <- getFieldOffsets(Table)]).
